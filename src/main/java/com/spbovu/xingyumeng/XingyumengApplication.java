aca95cd2373664af34e6b1a7279ab4a6b9f1af05
juyang <jy929746999@163.com>
2023-04-22 11:21:26 +0800

    自动生成版本

---
+++ src/main/java/com/spbovu/xingyumeng/XingyumengApplication.java
@@ -0,0 +1,13 @@
+package com.spbovu.xingyumeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XingyumengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XingyumengApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spbovu/xingyumeng/XingyumengApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spbovu.xingyumeng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XingyumengApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
